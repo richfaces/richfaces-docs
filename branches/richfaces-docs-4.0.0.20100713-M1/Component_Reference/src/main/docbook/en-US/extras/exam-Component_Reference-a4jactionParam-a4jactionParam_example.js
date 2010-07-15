79e2b268d823a303668b60d3141243ec3c82340b
Jay Balunas <jbalunas@redhat.com>
2010-07-15 18:42:05 +0000

    Created Milestone1 branch

---
+++ branches/richfaces-docs-4.0.0.20100713-M1/Component_Reference/src/main/docbook/en-US/extras/exam-Component_Reference-a4jactionParam-a4jactionParam_example.js
@@ -0,0 +1,11 @@
+public class ActionparamBean {
+    private String name = "John";
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
---
+++ branches/richfaces-docs-4.0.0.20100713-M1/Developer_Guide/src/main/docbook/en-US/extras/exam-Developer_Guide-Advanced_features-Example_request_error.js
@@ -0,0 +1,3 @@
+A4J.AJAX.onError = function(req, status, message){
+    window.alert("Custom onError handler "+message);
+}
