1c2b299c15e71684476bd12040c361c01a35752f
Alex Kolonitsky <akolonitsky@exadel.com>
2010-07-30 19:53:36 +0000

    change svn structure 4.0

--- branches/richfaces-docs-4.0.0.Alpha2/Developer_Guide/en-US/extras/exam-Developer_Guide-Advanced_features-Example_session_expiration_error.js
+++
@@ -1,7 +0,0 @@
-A4J.AJAX.onExpired = function(loc, expiredMsg){
-    if(window.confirm("Custom onExpired handler "+expiredMsg+" for a location: "+loc)){
-        return loc;
-    } else {
-        return false;
-    }
-}
--- tags/richfaces-docs-4.0.0.20100713-M1/Developer_Guide/src/main/docbook/en-US/extras/exam-Developer_Guide-Advanced_features-Example_request_error.js
+++
@@ -1,3 +0,0 @@
-A4J.AJAX.onError = function(req, status, message){
-    window.alert("Custom onError handler "+message);
-}
