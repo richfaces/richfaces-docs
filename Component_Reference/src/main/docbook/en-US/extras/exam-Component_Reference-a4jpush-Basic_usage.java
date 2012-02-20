709d1757722556d03a869d582fe9401e3016e9c6
Lukas Fryc <lfryc@redhat.com>
2012-02-20 18:27:28 +0100

    RFPL-1980: Improve a4j:push Docs for consistency, simplicity and readability

--- Component_Reference/src/main/docbook/en-US/extras/exam-Component_Reference-a4jpush-Basic_usage.java
+++ Component_Reference/src/main/docbook/en-US/extras/exam-Component_Reference-a4jpush-Basic_usage.java
@@ -1,3 +1,4 @@
+TopicConnection connection;
 TopicSession session;
 TopicPublisher publisher;
 
@@ -5,4 +6,21 @@ public void sendCurrentDate() throws JMSException {
     String currentDate = new Date().toString();
     ObjectMessage message = session.createObjectMessage(message);
     publisher.publish(message);
+}
+
+// messaging needs to be initialized before using method #sendCurrentDate()
+private void initializeMessaging() throws JMSException, NamingException {
+    if (connection == null) {
+        TopicConnectionFactory tcf = (TopicConnectionFactory) InitialContext.doLookup("java:/ConnectionFactory");
+        connection = tcf.createTopicConnection();
+    }
+    if (session == null) {
+        session = connection.createTopicSession(false, Session.AUTO_ACKNOWLEDGE);
+    }
+    if (topic == null) {
+        topic = InitialContext.doLookup("topic/datePush");
+    }
+    if (publisher == null) {
+        publisher = session.createPublisher(topic);
+    }
 }
