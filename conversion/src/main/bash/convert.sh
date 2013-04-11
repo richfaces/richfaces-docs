8ce20763844a672fcf50785336bb827043ea9020
Brian Leathem <bleathem@gmail.com>
2013-04-11 11:58:03 -0700

    Changed to using a docbook xml file created my manually running aasciidoctor (while waiting for upstream changes)

--- conversion/src/main/bash/convert.sh
+++ conversion/src/main/bash/convert.sh
@@ -3,6 +3,7 @@
 CONVERT_DIR=`dirname $(readlink -f $0)`
 SAXON_JAR=$CONVERT_DIR/../xslt/d2a/saxon9he.jar
 D2A_XSLT=$CONVERT_DIR/../xslt/d2a/d2a.xsl
+MY_JAVA_OPTS="-Xmx1024m -XX:MaxPermSize=512m"
 
 convert_dev_guide() {
     echo "Conversion started: Developer Guide"
@@ -13,7 +14,7 @@ convert_dev_guide() {
     DEV_GUIDE_AD=$DEV_GUIDE_DIR/Developer_Guide.asciidoc
     DEV_GUIDE_CONVERSION_LOG=$DEV_GUIDE_DIR/Developer_Guide.a2d.log
 
-    java -jar $SAXON_JAR -xi -s:$DEV_GUIDE_XML -o:$DEV_GUIDE_AD $D2A_XSLT &> $DEV_GUIDE_CONVERSION_LOG
+    java $MY_JAVA_OPTS -jar $SAXON_JAR -xi -s:$DEV_GUIDE_XML -o:$DEV_GUIDE_AD $D2A_XSLT &> $DEV_GUIDE_CONVERSION_LOG
     cat $DEV_GUIDE_CONVERSION_LOG
 
     date
@@ -29,7 +30,7 @@ convert_comp_ref() {
     COMP_REF_AD=$COMP_REF_DIR/Component_Reference.asciidoc
     COMP_REF_CONVERSION_LOG=$COMP_REF_DIR/Component_Reference.a2d.log
 
-    java -jar $SAXON_JAR -xi -s:$COMP_REF_XML -o:$COMP_REF_AD $D2A_XSLT &> $COMP_REF_CONVERSION_LOG
+    java $MY_JAVA_OPTS -jar $SAXON_JAR -xi -s:$COMP_REF_XML -o:$COMP_REF_AD $D2A_XSLT &> $COMP_REF_CONVERSION_LOG
     cat $COMP_REF_CONVERSION_LOG
 
     date
