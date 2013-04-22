dd5b0e62814780234dc9939e2f7e8974c4b24174
Brian Leathem <bleathem@gmail.com>
2013-04-22 15:53:40 -0700

    Changed the asciidoc script to use the asciidoctor implementation

--- conversion/src/main/bash/asciidoc.sh
+++ conversion/src/main/bash/asciidoc.sh
@@ -1,5 +1,7 @@
+
 #!/bin/bash
 
+ASCIIDOC_IMPL="$(readlink -f $(which asciidoctor))"
 CONVERT_DIR=`dirname $(readlink -f $0)`
 SAXON_JAR=$CONVERT_DIR/../xslt/d2a/saxon9he.jar
 D2A_XSLT=$CONVERT_DIR/../xslt/d2a/d2a.xsl
@@ -10,8 +12,9 @@ convert_dev_guide() {
     DEV_GUIDE_XML=$DEV_GUIDE_DIR/Developer_Guide.asciidoc.xml
     DEV_GUIDE_AD=$DEV_GUIDE_DIR/Developer_Guide.asciidoc
 
-    asciidoc -a docinfo -bdocbook45 -d book -o$DEV_GUIDE_XML $DEV_GUIDE_AD \
-             && xmllint --format $DEV_GUIDE_XML -o $DEV_GUIDE_XML
+    $ASCIIDOC_IMPL -a toc -a numbered -a docinfo -bdocbook -d book \
+                   -o$DEV_GUIDE_XML $DEV_GUIDE_AD \
+                && xmllint --format $DEV_GUIDE_XML -o $DEV_GUIDE_XML
 }
 
 convert_comp_ref() {
@@ -19,8 +22,9 @@ convert_comp_ref() {
     COMP_REF_XML=$COMP_REF_DIR/Component_Reference.asciidoc.xml
     COMP_REF_AD=$COMP_REF_DIR/Component_Reference.asciidoc
 
-    asciidoc -a docinfo -bdocbook45 -d book -o$COMP_REF_XML $COMP_REF_AD \
-             && xmllint --format $COMP_REF_XML -o $COMP_REF_XML
+    $ASCIIDOC_IMPL -a toc -a numbered -a docinfo -bdocbook -d book \
+                   -o$COMP_REF_XML $COMP_REF_AD \
+                && xmllint --format $COMP_REF_XML -o $COMP_REF_XML
 }
 
 convert_dev_guide
